d880e468fd7cf68cb29b9237e2be1b16e6ff4ce3
Pepped <36749464+Pepped@users.noreply.github.com>
2018-04-27 18:41:56 +0200

    Add files via upload

--- test/main.cpp
+++ test/main.cpp
@@ -14,28 +14,32 @@ int main()
     std::ifstream t("jsontest.txt");
     if (t.is_open()) {
         
-        db1.load(t, false, false);
+        db1.load(t, true,true);
         SignalK::DataBase db2 = db1;
         cout << db2;
         cin.get();
-        cout << endl;
+        cout<<"SUBTREE"<<endl;
         cout << db2.subtree("vessels.urn:mrn:signalk:uuid:705f5f1a-efaf-44aa-9cb8-a0fd6305567c.navigation.position");
         cin.get();
+        cin.get();
+        cout<<"vesselPROP"<<endl;
         cout << db2.getVesselProperty("urn:mrn:signalk:uuid:705f5f1a-efaf-44aa-9cb8-a0fd6305567c", "navigation.position");
         cin.get();
+        cout<<"GETSOURCE"<<endl;
         cout << db2.getSource("ttyUSB0");
         cin.get();
         SignalK::DataBase db3("urn:mrn:signalk:uuid:705f5f1a-efaf-44aa-9cb8-a0fd6305567c", "v1.0.0");
+        cout<<"STM JSON"<<endl;
         cout << db3.toJson();
         cin.get();
+        cout<<"SELF_free"<<endl;
+        cout<<db3<<endl;
+        cin.get();
+        cout<<endl;
+        cout<<"SELF"<<endl;
         cout << db3.getSelf()<< " " << db3.getVersion()<< endl;
-        cin.get();
-        std::ifstream d("jsondelta.txt");
-        std::string delta((std::istreambuf_iterator<char>(d)),
-                          std::istreambuf_iterator<char>());
-        db2.update(delta);
-        cout << db2.subtree("");
-        cin.get();
+        cout<<"close program"<<endl;
+
     }
 
     return 0;
