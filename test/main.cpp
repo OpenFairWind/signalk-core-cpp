9df6372da5353e5f7096f65ef697e605db66a8a6
Pepped <36749464+Pepped@users.noreply.github.com>
2018-05-07 10:23:02 +0200

    Add files via upload

--- test/main.cpp
+++ test/main.cpp
@@ -11,13 +11,14 @@ int main()
 {
 
     SignalK::DataBase db1;
-    std::ifstream t("jsontest.txt");
+    std::ifstream t("jfull.txt");
     if (t.is_open()) {
         
-        db1.load(t, true,true);
+        db1.load(t, false,false);
         SignalK::DataBase db2 = db1;
-        cout << db2;
-        cin.get();
+        //cout << db2;
+        cout << db2.subtree("");
+       /* cin.get();
         cout<<"SUBTREE"<<endl;
         cout << db2.subtree("vessels.urn:mrn:signalk:uuid:705f5f1a-efaf-44aa-9cb8-a0fd6305567c.navigation.position");
         cin.get();
@@ -39,6 +40,20 @@ int main()
         cout<<"SELF"<<endl;
         cout << db3.getSelf()<< " " << db3.getVersion()<< endl;
         cout<<"close program"<<endl;
+    */
+        SignalK::DataBase db4("urn:mrn:signalk:uuid:705f5f1a-efaf-44aa-9cb8-a0fd6305567c", "v1.0.0");
+      //  cout << db4.toJson();
+        cin.get();
+        //cout << db4.getSelf()<< " " << db4.getVersion()<< endl;
+        cin.get();
+        std::ifstream d("jdelta.txt");
+        std::string delta((std::istreambuf_iterator<char>(d)),
+                          std::istreambuf_iterator<char>());
+        db2.update(delta);
+        cout << db2.subtree("");
+        cin.get();
+
+        cout<< db2;
 
     }
 
